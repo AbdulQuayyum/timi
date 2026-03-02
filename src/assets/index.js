6f779a5330796954de56d4da80c9b93b692c9197
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2026-03-02 22:48:22 +0100

    (feat) added images....

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,28 @@ import profile from "./images/profile.png"
 import timiyoung from "./images/timiyoung.png"
 import ty from "./images/ty.png"
 
+import adobexd from './images/adobe-xd.jpg'
+import canva from './images/canva.jpg'
+import corelDraw from './images/corel-draw.jpg'
+import figma from './images/figma.jpg'
+import framer from './images/framer.jpg'
+import illustrator from './images/illustrator.jpg'
+import javascript from './images/javascript.jpg'
+import nextJs from './images/next-js.jpg'
+import photoshop from './images/photoshop.jpg'
+import reactNative from './images/react-native.jpg'
+import react from './images/react.jpg'
+import sketch from './images/sketch.jpg'
+import tailwind from './images/tailwind.jpg'
+
+import timi from './images/timi.png'
+
+import pattern01 from './images/pattern-01.jpg'
+import pattern02 from './images/pattern-02.jpg'
+import pattern03 from './images/pattern-03.jpg'
+import pattern04 from './images/pattern-04.jpg'
+import pattern05 from './images/pattern-05.jpg'
+
 import work01 from "./images/work-01.png"
 import work02 from "./images/work-02.png"
 import work03 from "./images/work-03.png"
@@ -70,12 +92,24 @@ import ologe16 from "./images/ologe/16.png"
 import ologe17 from "./images/ologe/17.png"
 import ologe18 from "./images/ologe/18.png"
 
+import dextreMockup01 from './images/dextre/mockup-01.png'
+import dextreMockup02 from './images/dextre/mockup-02.png'
+import dextreMockup03 from './images/dextre/mockup-03.png'
+import dextreMockup04 from './images/dextre/mockup-04.png'
+import dextreMockup05 from './images/dextre/mockup-05.png'
+
+import dextreScreen01 from './images/dextre/screen-01.png'
+import dextreScreen02 from './images/dextre/screen-02.png'
+import dextreScreen03 from './images/dextre/screen-03.png'
 
 export default {
     hero01, hero02, hero03, hero04, hero05,
-    profile, timiyoung, ty,
+    profile, timiyoung, ty, timi,
+    adobexd, canva, corelDraw, figma, framer, illustrator, javascript, nextJs, photoshop, reactNative, react, sketch, tailwind,
     work01, work02, work03, work04, work05, work06, work07, work08, work09, work10, work11, work12, work13,
     marsone01, marsone02, marsone03, marsone04, marsone05, marsone06, marsone07, marsone08, marsone09, marsone10, marsone11, marsone12, marsone13, marsone14, marsone15, marsone16, marsone17, marsone18, marsone19,
     zenthom01, zenthom02, zenthom03, zenthom04, zenthom05, zenthom06, zenthom07, zenthom08,
-    ologe01, ologe02, ologe03, ologe04, ologe05, ologe06, ologe07, ologe08, ologe09, ologe10, ologe11, ologe12, ologe13, ologe14, ologe15, ologe16, ologe17, ologe18
+    ologe01, ologe02, ologe03, ologe04, ologe05, ologe06, ologe07, ologe08, ologe09, ologe10, ologe11, ologe12, ologe13, ologe14, ologe15, ologe16, ologe17, ologe18,
+    dextreMockup01, dextreMockup02, dextreMockup03, dextreMockup04, dextreMockup05, dextreScreen01, dextreScreen02, dextreScreen03,
+    pattern01, pattern02, pattern03, pattern04, pattern05
 }
